74ab0a78589be7bdc8650b2cbbd80f20db2539e4
LorenaL567 <lorena.lici579@gmail.com>
2023-11-08 18:55:03 +0100

    fixed claim status

--- src/components/FARMINNG/constant-staking-dai-front.js
+++ src/components/FARMINNG/constant-staking-dai-front.js
@@ -1202,17 +1202,30 @@ const StakeEthDai = ({
                 /> */}
                   </div>
                   <div className="claim-reinvest-container d-flex justify-content-between align-items-center gap-3">
-                    <button
+                  <button
                       disabled={
-                        claimStatus === "claimed" ||
-                        claimStatus === "success" ||
-                        pendingDivs <= 0
-                          ? true
+                        claimStatus === "claimed" || claimStatus === "success"
+                          ? // || pendingDivs <= 0
+                            true
                           : false
                       }
-                      className={`btn disabled-btn`}
+                      className={`btn filledbtn ${
+                        claimStatus === "claimed" && claimStatus === "initial"
+                          ? // ||  pendingDivs <= 0
+                            "disabled-btn"
+                          : claimStatus === "failed"
+                          ? "fail-button"
+                          : claimStatus === "success"
+                          ? "success-button"
+                          : null
+                      } d-flex justify-content-center align-items-center gap-2`}
                       style={{ height: "fit-content" }}
-                      onClick={handleClaimDivs}
+                      // onClick={handleClaimDivs}
+                      onClick={() => {
+                        window.$.alert(
+                          "*The rewards earned from the day of the migration until the expiration of the contracts will be distributed to the users automatically at the end of the contract."
+                        );
+                      }}
                     >
                       {claimLoading ? (
                         <div
