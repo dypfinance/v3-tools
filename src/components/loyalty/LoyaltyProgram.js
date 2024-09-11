2d6091373e358ecda2842e18652f986c67467930
AldiAlinj <aldialinj0@gmail.com>
2024-09-11 10:25:46 +0200

    finished loyalty program apis

--- src/components/loyalty/LoyaltyProgram.js
+++ src/components/loyalty/LoyaltyProgram.js
@@ -16,10 +16,97 @@ import coinbase from "./assets/coinbase.png";
 import coin98 from "./assets/coin98.png";
 import trustwallet from "./assets/trustwallet.png";
 import safepal from "./assets/safepal.png";
+import axios from "axios";
+import { shortAddress } from "../../functions/shortAddress";
 
-const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
+const LoyaltyProgram = ({ coinbase, isConnected, handleConnection }) => {
+  const baseUrl = "https://api.worldofdypians.com/api";
+
   const [popup, setPopup] = useState(false);
   const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    emailAddress: "",
+    twitterUser: "",
+  });
+  const [latestUsers, setLatestUsers] = useState([]);
+  const [user, setUser] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const fetchLatestUsers = async () => {
+    await axios
+      .get(`${baseUrl}/loyalty/latest`)
+      .then((data) => {
+        setLatestUsers(data.data.participants);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const loyaltyCheck = async () => {
+    await axios
+      .get(`${baseUrl}/loyalty/check/${coinbase}`)
+      .then((data) => {
+        setUser(data.data.user);
+        if (data.data.user) {
+          setStep(5);
+        } else {
+          setStep(3);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleEmailChange = (e) => {
+    setFormData({
+      ...formData,
+      emailAddress: e.target.value, // Update only the email field
+    });
+  };
+
+  // Handler for the twitter input change
+  const handleTwitterChange = (e) => {
+    setFormData({
+      ...formData,
+      twitterUser: e.target.value, // Update only the twitter field
+    });
+  };
+
+  const getTimeAgo = (timestamp) => {
+    const now = new Date();
+    const past = new Date(timestamp);
+    const diffInSeconds = Math.floor((now - past) / 1000); // Difference in seconds
+
+    if (diffInSeconds < 60) {
+      return `${diffInSeconds} seconds ago`;
+    } else if (diffInSeconds < 3600) {
+      const minutes = Math.floor(diffInSeconds / 60);
+      return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+    } else if (diffInSeconds < 86400) {
+      const hours = Math.floor(diffInSeconds / 3600);
+      return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+    } else if (diffInSeconds < 604800) {
+      const days = Math.floor(diffInSeconds / 86400);
+      return `${days} ${days === 1 ? "day" : "days"} ago`;
+    } else if (diffInSeconds < 2419200) {
+      const weeks = Math.floor(diffInSeconds / 604800);
+      return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`;
+    } else {
+      const months = Math.floor(diffInSeconds / 2419200);
+      return `${months} ${months === 1 ? "month" : "months"} ago`;
+    }
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    await axios
+      .post(`${baseUrl}/loyalty/add`, { ...formData, walletAddress: coinbase })
+      .then((res) => {
+        setLoading(false);
+        setRefresh(true);
+        setStep(4);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const html = document.querySelector("html");
 
@@ -31,12 +118,15 @@ const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
     }
   }, [popup]);
 
+  useEffect(() => {
+    if (coinbase && isConnected) {
+      loyaltyCheck();
+    }
+  }, [coinbase, isConnected]);
 
   useEffect(() => {
-   if(coinbase && isConnected){
-    setStep(3)
-   }
-  }, [coinbase, isConnected])
+    fetchLatestUsers();
+  }, [refresh])
   
 
   return (
@@ -70,9 +160,9 @@ const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
               </div>
             </div>
           </div>
-          <div className="col-12 col-lg-6">
-            <div className="loyalty-container p-3">
-              <div className="row">
+          <div className="col-12 col-lg-6 ">
+            <div className="loyalty-container p-3 h-100">
+              <div className="row h-100">
                 <div className="col-12 col-lg-5 d-flex align-items-end">
                   <div className="d-flex flex-column gap-3">
                     <h6 className="loyalty-title mb-0">90 days gas free</h6>
@@ -111,7 +201,7 @@ const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
                 </div>
                 <div className="col-12 col-lg-7">
                   <div
-                    className="reimbursement-wrapper p-3 d-flex flex-column align-items-center gap-2"
+                    className="reimbursement-wrapper h-100 p-3 d-flex flex-column align-items-center gap-2"
                     style={{ borderRadius: "12px" }}
                   >
                     <h6 className="participants-title mb-0">Participants</h6>
@@ -124,55 +214,22 @@ const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
                       </span>
                     </div>
                     <div className="d-flex flex-column gap-2 w-100">
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
-                      <div className="participant-item d-flex align-items-center justify-content-between w-100 py-1">
-                        <span className="participant-name">
-                          0x66...f506 joined the{" "}
-                          <span style={{ color: "#fff" }}>Loyalty Program</span>
-                        </span>
-                        <span className="participant-time-ago">3 min ago</span>
-                      </div>
+                      {latestUsers.map((item, index) => (
+                        <div
+                          key={index}
+                          className="participant-item d-flex align-items-center justify-content-between w-100 py-1"
+                        >
+                          <span className="participant-name">
+                            {shortAddress(item.walletAddress)} joined the{" "}
+                            <span style={{ color: "#fff" }}>
+                              Loyalty Program
+                            </span>
+                          </span>
+                          <span className="participant-time-ago">
+                            {getTimeAgo(item.timestamp)}
+                          </span>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
@@ -247,26 +304,43 @@ const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
           </>
         )}
         {step === 1 ? (
-          <button className="btn filled-btn" onClick={() => setStep(2)}>Connect wallet</button>
+          <button className="btn filled-btn" onClick={() => setStep(2)}>
+            Connect wallet
+          </button>
         ) : step === 2 ? (
           <div className="d-flex flex-column gap-2 w-100">
-            <div className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between" onClick={handleConnection}>
+            <div
+              className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between"
+              onClick={handleConnection}
+            >
               <span className="loyalty-wallet-title">Metamask</span>
               <img src={metamask} width={30} height={30} alt="" />
             </div>
-            <div className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between" onClick={handleConnection}>
+            <div
+              className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between"
+              onClick={handleConnection}
+            >
               <span className="loyalty-wallet-title">Coinbase</span>
               <img src={coinbase} width={30} height={30} alt="" />
             </div>
-            <div className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between" onClick={handleConnection}>
+            <div
+              className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between"
+              onClick={handleConnection}
+            >
               <span className="loyalty-wallet-title">Coin98</span>
               <img src={coin98} width={30} height={30} alt="" />
             </div>
-            <div className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between" onClick={handleConnection}>
+            <div
+              className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between"
+              onClick={handleConnection}
+            >
               <span className="loyalty-wallet-title">Trustwallet</span>
               <img src={trustwallet} width={30} height={30} alt="" />
             </div>
-            <div className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between" onClick={handleConnection}>
+            <div
+              className="connect-wallet-item p-3 w-100 d-flex align-items-center justify-content-between"
+              onClick={handleConnection}
+            >
               <span className="loyalty-wallet-title">SafePal</span>
               <img src={safepal} width={30} height={30} alt="" />
             </div>
@@ -278,49 +352,76 @@ const LoyaltyProgram = ({coinbase, isConnected, handleConnection}) => {
               <span className="loyalty-popup-span-2">0xB329...bDe8</span>
             </div>
             <div className="reimbursement-divider "></div>
-            <div className="d-flex flex-column gap-3 w-100">
+            <form className="d-flex flex-column gap-3 w-100">
               <span className="loyalty-popup-span">Other Details</span>
               <input
-                type="text"
+                type="email"
                 placeholder="Email Address"
                 className="loyalty-popup-input p-2"
+                value={formData.emailAddress}
+                onChange={handleEmailChange}
               />
               <input
                 type="text"
                 placeholder="Twitter username"
                 className="loyalty-popup-input p-2"
+                value={formData.twitterUser}
+                onChange={handleTwitterChange}
               />
-            </div>
+            </form>
             <div className="d-flex w-100 justify-content-center">
               <button
+                type="submit"
                 className="btn filled-btn"
                 style={{ width: "fit-content" }}
+                disabled={loading}
+                onClick={handleSubmit}
               >
-                Submit
+                {loading ? (
+                  <div
+                    class="spinner-border spinner-border-sm text-light"
+                    role="status"
+                  >
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                ) : (
+                  "Submit"
+                )}
               </button>
             </div>
           </>
         ) : step === 4 ? (
           <div className="d-flex flex-column w-100 align-items-center gap-3">
             <img src={successful} alt="" />
-            <p className="loyalty-popup-desc" style={{ textAlign: "center", width: "75%" }}>
+            <p
+              className="loyalty-popup-desc"
+              style={{ textAlign: "center", width: "75%" }}
+            >
               Congratulations! Your application for the Loyalty Program was
               successful. Please stay tuned to our official social media
               channels for the winners announcement.
             </p>
-            <h6 className="loyalty-popup-close" onClick={() => setPopup(false)}>Close</h6>
+            <h6 className="loyalty-popup-close" onClick={() => setPopup(false)}>
+              Close
+            </h6>
           </div>
-        ) : step === 5 ?  (
+        ) : step === 5 ? (
           <div className="d-flex flex-column w-100 align-items-center gap-3">
             <img src={denied} alt="" />
-            <p className="loyalty-popup-desc" style={{ textAlign: "center", width: "75%" }}>
-            Your application for the Loyalty Program has already been received. Please check back soon.
+            <p
+              className="loyalty-popup-desc"
+              style={{ textAlign: "center", width: "75%" }}
+            >
+              Your application for the Loyalty Program has already been
+              received. Please check back soon.
             </p>
-            <h6 className="loyalty-popup-close" onClick={() => setPopup(false)}>Close</h6>
+            <h6 className="loyalty-popup-close" onClick={() => setPopup(false)}>
+              Close
+            </h6>
           </div>
-          
-        ) : <></>
-      }
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
