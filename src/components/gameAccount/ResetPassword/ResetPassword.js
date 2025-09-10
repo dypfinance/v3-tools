29d4f1172bd5bb4d037228be224b1c05cfbad165
LorenaL57 <lorena.lici579@gmail.com>
2025-09-10 11:46:45 +0200

    added recaptcha

--- src/components/gameAccount/ResetPassword/ResetPassword.js
+++ src/components/gameAccount/ResetPassword/ResetPassword.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import LoginCard from "../LoginCard/LoginCard";
 import LoginWrapper from "../LoginWrapper/LoginWrapper";
 import Input from "../Input/Input";
@@ -7,7 +7,7 @@ import Button from "../Button/Button";
 import ErrorAlert from "../ErrorAlert/ErrorAlert";
 import { useNavigate } from "react-router-dom";
 import classes from "./ResetPassword.module.css";
-
+import ReCaptchaV2 from "react-google-recaptcha";
 import { encode } from "base-64";
 
 const ResetPassword = () => {
@@ -19,26 +19,36 @@ const ResetPassword = () => {
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
   const [isResetSuccess, setIsResetSuccess] = useState(false);
   const [error, setError] = useState(null);
+  const [captchaValue, setCaptchaValue] = useState(null);
+  const recaptchaRef = useRef(null);
+
+  const handleCaptchaChange = (value) => {
+    setCaptchaValue(value);
+  };
 
   const handleNewPassword = async () => {
-    if (newPassword === confirmNewPassword) {
-      try {
-        const recoverSession = encode(`${token}:${newPassword}`);
-        await axios.post(
-          "https://axf717szte.execute-api.eu-central-1.amazonaws.com/prod/auth/reset",
-          { recoverSession }
-        );
-        setIsResetSuccess(true);
-      } catch (error) {
-        if (error?.response?.data?.error?.name === "PasswordValidator") {
-          setError(
-            "The new password should have  a minimum length of 8 characters, 1 lowercase letter, 1 digit and 1 symbol."
+    if (!captchaValue) {
+      window.alertify.error("Please verify the reCAPTCHA");
+    } else {
+      if (newPassword === confirmNewPassword) {
+        try {
+          const recoverSession = encode(`${token}:${newPassword}`);
+          await axios.post(
+            "https://axf717szte.execute-api.eu-central-1.amazonaws.com/prod/auth/reset",
+            { recoverSession }
           );
-        } else {
-          if (error?.response?.data?.error?.message) {
-            setError(error?.response?.data?.error?.message);
+          setIsResetSuccess(true);
+        } catch (error) {
+          if (error?.response?.data?.error?.name === "PasswordValidator") {
+            setError(
+              "The new password should have  a minimum length of 8 characters, 1 lowercase letter, 1 digit and 1 symbol."
+            );
           } else {
-            setError("Something went wrong.");
+            if (error?.response?.data?.error?.message) {
+              setError(error?.response?.data?.error?.message);
+            } else {
+              setError("Something went wrong.");
+            }
           }
         }
       }
@@ -108,10 +118,18 @@ const ResetPassword = () => {
             }}
             inputType={"password"}
           />
+          <ReCaptchaV2
+            sitekey="6LfFVMQrAAAAAGauKrn5cyQZRaXHMMlHMUz9IOnu"
+            style={{ display: "inline-block" }}
+            theme="dark"
+            ref={recaptchaRef}
+            onChange={handleCaptchaChange}
+          />
           <Button
             style={{ margin: "auto", marginTop: 30, marginBottom: 20 }}
             onPress={handleNewPassword}
             title={"Confirm Password"}
+            disabled={!captchaValue}
           />
         </div>
       </LoginCard>
